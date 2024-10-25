5a3eba411ec797f461b85d2546cbc6a7057b46e3
Vova <linkdomru@gmail.com>
2024-10-25 14:34:45 +0700

    sort,split,forEach,for (let val of arr)

---
+++ 21massiv.js
@@ -0,0 +1,31 @@
+
+
+const arr = [2, 2, 3, 6, 8];
+
+for (let i = 0; i < arr.length; i++){
+	console.log(arr);
+	
+}
+
+// цикл for in
+
+for (let val of arr) {
+	console.log(val);
+cont
+	
+}
+
+arr.forEach(function (item, i, arr) {
+	console.log(`${i}: ${item} и массив ${arr}`);
+	
+});
+const str = prompt("", "");
+const product = str.split(",");
+console.log(product);
+product.sort();
+product.sort(com);
+console.log(product);
+function com(a, b) {
+	return a - b;
+}
+console.log(product);
